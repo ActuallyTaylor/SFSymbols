f8c193b14609f0616197f285f56e32fb9d079420
Taylor Lineman <32944568+ActuallyTaylor@users.noreply.github.com>
2023-08-06 15:39:08 -0400

    Formatting Changes

--- Sources/SFSymbols/Label+SFSymbol.swift
+++ Sources/SFSymbols/Label+SFSymbol.swift
@@ -2,23 +2,21 @@ import SwiftUI
 
 @available(iOS 14.0, *)
 public extension Label {
-  init(_ titleKey: LocalizedStringKey,
-       symbol: SFSymbol) where Title == Text, Icon == Image {
-    switch symbol {
-    case .custom(let name):
-      self = Label(titleKey, image: name)
-    default:
-      self = Label(titleKey, systemImage: symbol.name)
+    init(_ titleKey: LocalizedStringKey, symbol: SFSymbol) where Title == Text, Icon == Image {
+        switch symbol {
+        case .custom(let name):
+            self = Label(titleKey, image: name)
+        default:
+            self = Label(titleKey, systemImage: symbol.name)
+        }
     }
-  }
-
-  init<S>(_ title: S,
-          symbol: SFSymbol) where S: StringProtocol, Title == Text, Icon == Image {
-    switch symbol {
-    case .custom(let name):
-      self = Label(title, image: name)
-    default:
-      self = Label(title, systemImage: symbol.name)
+    
+    init<S>(_ title: S, symbol: SFSymbol) where S: StringProtocol, Title == Text, Icon == Image {
+        switch symbol {
+        case .custom(let name):
+            self = Label(title, image: name)
+        default:
+            self = Label(title, systemImage: symbol.name)
+        }
     }
-  }
 }
